7707b42e3729cedd5371192236e9e7120406ae29
CodingKaiser <falkonoe@live.com>
2017-03-01 09:31:43 -0800

    Refactored some functions and added comments.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 					 ["Lead singer Colin Meloy married Carson Ellis, who did <b>what</b> for the band?", 1, ["Managed the band in its infancy", "Draws the band's album art", "Served as the band's first drummer", "Having dated its bassist"], "<img src='assets/images/q4Image.gif' class='center-block question-graphic thumbnail'>"],
 					 ["<u>Complete the lyric</u>: But never once in the employ Of these holy men Did I ever once turn my mind from the thought of...", 3, ["Grizelda", "Lust", "You", "Revenge"], "<img src='assets/images/q5Image.gif' class='center-block question-graphic thumbnail'>"]
 					];
+	var mainContentArea = $("#main-content-area");
 	var delay;
 	var timer;
 
@@ -23,6 +24,7 @@ $(document).ready(function() {
 	};
 
 	function resetGame() {
+		// Reset game stats
 		game.wins = 0;
 		game.losses = 0;
 		game.skipped = 0;
@@ -30,14 +32,17 @@ $(document).ready(function() {
 	};
 
 	function clearGameArea() {
-		$("#main-content-area").children().each(function() {
+		// Remove all of the divs in the main content div
+		mainContentArea.children().each(function() {
 			$(this).remove();
 		});
 	};
 
 	function stageNextQuestion() {
+		// Clear intervals if any have been set
 		clearInterval(timer);
 		clearTimeout(delay);
+		// Reset timer
 		game.secondsRemaining = game.maxSeconds;
 		clearGameArea();
 		populateGameArea();
@@ -46,19 +51,23 @@ $(document).ready(function() {
 	};
 
 	function populateGameArea() {
-		$("#main-content-area").append("<h2 id='time-counter' class='text-center'>Time Remaining: " + game.secondsRemaining + " Seconds</h2>");
-		$("#main-content-area").append("<h2 id='question' class='text-center'>" + getCurrentQuestion() + "</h2>");
+		mainContentArea.append("<h2 id='time-counter' class='text-center'>Time Remaining: " + 
+										game.secondsRemaining + " Seconds</h2>");
+		mainContentArea.append("<h2 id='question' class='text-center'>" + 
+										getCurrentQuestion() + "</h2>");
+		// Loop through questions and add them to the main content area
 		for (var i = 0; i < getCurrentQuestionAnswers().length; i++) {
 			var answerDisplay = $("<h3></h3>");
 			answerDisplay.attr("id", "answer" + i);
 			answerDisplay.addClass("answer center-block");
 			answerDisplay.text(getCurrentQuestionAnswers()[i]);
-			$("#main-content-area").append(answerDisplay);
+			mainContentArea.append(answerDisplay);
 		}
 	};
 
 	function hookUpClickHandlers() {
-		$("#main-content-area").children(".answer").each(function() {
+		// Clicking on answers takes user to next screen
+		mainContentArea.children(".answer").each(function() {
 			$(this).on("click", function() {
 				goToAnswerScreen(true, $(this).attr("id"));
 			});
@@ -67,30 +76,45 @@ $(document).ready(function() {
 
 	function goToAnswerScreen(userGaveAnswer, idOfAnswer) {
 		clearGameArea();
+		// If user gave answer, evaluate correctness.
+		// Otherwise, tally up 'skipped' count
 		if (userGaveAnswer) {
 			var answerIdDigit = parseInt(idOfAnswer[idOfAnswer.length-1]);
 			if (answerIdDigit === getCurrentQuestionAnswerIndex()) {
-				game.wins++;
-				(new Audio('assets/sounds/victory.wav')).play();
-				$("#main-content-area").append("<h3 class='text-center'>You Win!</h3>");
+				playerGaveCorrectAnswer();
 			} else {
-				(new Audio('assets/sounds/defeat.wav')).play();
-				game.losses++;
-				$("#main-content-area").append("<h3 class='text-center'>You Lose!</h3>");
-				$("#main-content-area").append("<h3 class='text-center'>The correct answer was: '" + getCurrentQuestionAnswer() + "'</h3>");
+				playerGaveWrongAnswer();
 			}
 		} else {
-			(new Audio('assets/sounds/outatime.wav')).play();
-			game.skipped++;
-			$("#main-content-area").append("<h3 class='text-center'>Time ran out!</h3>");
-			$("#main-content-area").append("<h3 class='text-center'>The correct answer was: '" + getCurrentQuestionAnswer() + "'</h3>");
+			playerSkippedQuestion();
 		}
-		$("#main-content-area").append(getCurrentQuestionGraphic());
+		mainContentArea.append(getCurrentQuestionGraphic());
 		delay = setTimeout(nextQuestionOrFinishGame, 4000);
+	}
+
+	function playerGaveCorrectAnswer() {
+		game.wins++;
+		(new Audio('assets/sounds/victory.wav')).play();
+		mainContentArea.append("<h3 class='text-center'>You Win!</h3>");
+	}
+
+	function playerGaveWrongAnswer() {
+		(new Audio('assets/sounds/defeat.wav')).play();
+		game.losses++;
+		mainContentArea.append("<h3 class='text-center'>You Lose!</h3>");
+		mainContentArea.append("<h3 class='text-center'>The correct answer was: '" + getCurrentQuestionAnswer() + "'</h3>");
+	}
+
+	function playerSkippedQuestion() {
+		(new Audio('assets/sounds/outatime.wav')).play();
+		game.skipped++;
+		mainContentArea.append("<h3 class='text-center'>Time ran out!</h3>");
+		mainContentArea.append("<h3 class='text-center'>The correct answer was: '" + getCurrentQuestionAnswer() + "'</h3>");
 	}
 
 	function nextQuestionOrFinishGame() {
 		game.currentQuestionIndex++;
+		// If at end of quiz, go to 'finish game' screen
 		if (game.currentQuestionIndex < questions.length) {
 			stageNextQuestion();
 		} else {
@@ -100,11 +124,13 @@ $(document).ready(function() {
 
 	function finishGame() {
 		clearGameArea();
-		$("#main-content-area").append("<h2 class='text-center'>All done! Here's how you did:</h2>")
-		$("#main-content-area").append("<h3 class='text-center'>Correct Answers: " + game.wins + "</h3>");
-		$("#main-content-area").append("<h3 class='text-center'>Incorrect Answers: " + game.losses + "</h3>");
-		$("#main-content-area").append("<h3 class='text-center'>Skipped Questions: " + game.skipped + "</h3>");
-		$("#main-content-area").append("<button id='start-game-btn' type='button' class='btn btn-default center-block'>Restart Game</button>");
+		// Display summary stats in main content area
+		mainContentArea.append("<h2 class='text-center'>All done! Here's how you did:</h2>")
+		mainContentArea.append("<h3 class='text-center'>Correct Answers: " + game.wins + "</h3>");
+		mainContentArea.append("<h3 class='text-center'>Incorrect Answers: " + game.losses + "</h3>");
+		mainContentArea.append("<h3 class='text-center'>Skipped Questions: " + game.skipped + "</h3>");
+		mainContentArea.append("<button id='start-game-btn' type='button' class='btn btn-default center-block'>Restart Game</button>");
+		// Give player option to restart game
 		$("#start-game-btn").click(start);
 	}
 
@@ -138,7 +164,8 @@ $(document).ready(function() {
 			goToAnswerScreen(false, "");
 		} else {
 			game.secondsRemaining--;
-			$("#time-counter").text("Time Remaining: " + game.secondsRemaining + " Seconds");
+			$("#time-counter").text("Time Remaining: " + 
+									game.secondsRemaining + " Seconds");
 		}
 	};
 
